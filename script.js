353d68a2b2c774cd8867ba92d7d19e7cac24b8ea
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-29 17:23:38 -0300

    Video 4

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-function draw(){
+
+function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     x += dx;
     y += dy;
 } 
