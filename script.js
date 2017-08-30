037e598a688da5fd3ee802b96df10190daf27901
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-29 21:18:58 -0300

    Collision detection

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ var x = canvas.width/2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
+
 
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
@@ -21,6 +23,15 @@ function drawBall(){
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    if(y + dy < 0 || y + dy > canvas.height){
+        dy = -dy;
+    }
+
+    if(x + dx < 0 || x + dx > canvas.width){
+        dx = -dx
+    }
+
     x += dx;
     y += dy;
 } 
